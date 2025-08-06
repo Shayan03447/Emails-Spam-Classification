64cd85eec447ed568a22a3f27653c87e8e220a28
Shayan <shanich03447@gmail.com>
2025-08-06 16:55:28 +0500

    final work

--- log/model_evaluation.py
+++ log/model_evaluation.py
@@ -10,3 +10,11 @@
 2025-08-06 12:48:58,174 - model_building - DEBUG - Data loaded from: ./data/processed/test_tfidf.csv
 2025-08-06 12:48:58,295 - model_building - DEBUG - Model evaluation metrics calculated
 2025-08-06 12:48:58,381 - model_building - DEBUG - Metrics saved to: reports/metrics.json
+2025-08-06 14:35:32,429 - model_building - DEBUG - Model loading from: ./models/model.pkl
+2025-08-06 14:35:32,445 - model_building - DEBUG - Data loaded from: ./data/processed/test_tfidf.csv
+2025-08-06 14:35:32,507 - model_building - DEBUG - Model evaluation metrics calculated
+2025-08-06 14:35:32,513 - model_building - DEBUG - Metrics saved to: reports/metrics.json
+2025-08-06 14:52:36,938 - model_building - DEBUG - Model loading from: ./models/model.pkl
+2025-08-06 14:52:36,959 - model_building - DEBUG - Data loaded from: ./data/processed/test_tfidf.csv
+2025-08-06 14:52:37,029 - model_building - DEBUG - Model evaluation metrics calculated
+2025-08-06 14:52:37,031 - model_building - DEBUG - Metrics saved to: reports/metrics.json
