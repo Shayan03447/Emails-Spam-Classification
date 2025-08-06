66427a369319a61c38988777ae97d3143318b783
Shayan <shanich03447@gmail.com>
2025-08-06 11:30:30 +0500

    pipeline configure using dvc without params

--- log/model_evaluation.py
+++ log/model_evaluation.py
@@ -2,3 +2,7 @@
 2025-08-06 11:00:45,402 - model_building - DEBUG - Data loaded from: ./data/processed/test_tfidf.csv
 2025-08-06 11:00:45,488 - model_building - DEBUG - Model evaluation metrics calculated
 2025-08-06 11:00:45,491 - model_building - DEBUG - Metrics saved to: reports/metrics.json
+2025-08-06 11:25:16,630 - model_building - DEBUG - Model loading from: ./models/model.pkl
+2025-08-06 11:25:16,668 - model_building - DEBUG - Data loaded from: ./data/processed/test_tfidf.csv
+2025-08-06 11:25:16,768 - model_building - DEBUG - Model evaluation metrics calculated
+2025-08-06 11:25:16,771 - model_building - DEBUG - Metrics saved to: reports/metrics.json
